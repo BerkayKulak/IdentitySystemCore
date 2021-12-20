d28aeceda0273943433551b1a12b2fe07cf051be
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-20 01:02:39 -0700

    Asp.Net Core Identity'nin kurulması

---
+++ IdentitySystemCore/Models/AppIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentitySystemCore.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        // IdentityDbContext'in ctoruna DbContextOptions<AppIdentityDbContext> değerini gönderdim.
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
