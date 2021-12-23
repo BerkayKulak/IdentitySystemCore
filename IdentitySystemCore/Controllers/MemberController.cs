9425babc061ff5767320487b7c205a9e896d29a3
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2021-12-23 05:38:24 -0700

    Üye anasayfanın tasarlanması/kodlanması

--- IdentitySystemCore/Controllers/MemberController.cs
+++ IdentitySystemCore/Controllers/MemberController.cs
@@ -3,16 +3,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentitySystemCore.Models;
+using IdentitySystemCore.ViewModels;
+using Mapster;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace IdentitySystemCore.Controllers
 {
-    
+
+    //[Authorize]// membercontrollere sadece üyeler erişecek.
     public class MemberController : Controller
     {
+        public UserManager<AppUser> userManager { get; }
+        public SignInManager<AppUser> signInManager { get; }
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            // kullanıcı bir siteye geldiği zaman, üye de olsa olmasada user classı oluşturur
+            // bir tane kimliği oluşur, eğer kullanıcı giriş yapmamışsa isauthenticate false olur.
+            // boş bir kimlik oluşur. login olursa biz name,usernama gibi alanları yakalayabiliriz.
+            //User.Identity.
+
+
+            AppUser user = userManager.FindByNameAsync(User.Identity.Name).Result;
+            // userin içindeki propertylerden UserViewModel içerisindeki Propertyler ile eşleşenleri
+            // userViewModel'e aktaracak
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+
+            //UserViewModel userViewModel = new UserViewModel();
+            //userViewModel.UserName = user.UserName;
+
+
+            return View(userViewModel);
         }
     }
 }
