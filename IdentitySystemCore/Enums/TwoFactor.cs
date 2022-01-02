a9d4aaf3e69d46eba8e8e1deb983f1380e333bae
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-02 03:29:27 -0700

    TwoFactor enum tipinin oluşturulması

---
+++ IdentitySystemCore/Enums/TwoFactor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentitySystemCore.Enums
+{
+    public enum TwoFactor
+    {
+        [Display(Name = "Hiç Biri")]
+        None=0,
+        [Display(Name = "Telefon İle Kimlik Doğrulama")]
+        Phone = 1,
+        [Display(Name = "Email ile Kimlik Doğrulama")]
+        Email = 2,
+        [Display(Name = "Microsoft/Goole Authtenticator İle Kimlik Doğrulama")]
+        MicrosoftGoogle =3
+    }
+}
