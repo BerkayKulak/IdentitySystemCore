a8cf50bdd3bb6726e37e95c5253b44b8a35509b0
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-04 12:17:56 -0700

    SmsSender.cs dosyasının kodlanması

---
+++ IdentitySystemCore/TwoFactorService/SmsSender.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace IdentitySystemCore.TwoFactorService
+{
+    public class SmsSender
+    {
+        private readonly TwoFactorOptions _twoFactorOptions;
+        private readonly TwoFactorService _twoFactorService;
+
+        public SmsSender(IOptions<TwoFactorOptions> options, TwoFactorService twoFactorService)
+        {
+            _twoFactorOptions = options.Value;
+            _twoFactorService = twoFactorService;
+        }
+
+        public string Send(string phone)
+        {
+            string code = _twoFactorService.GetCodeVerification().ToString();
+
+            // SMS PROVIDER
+
+
+            return code;
+        }
+    }
+}
