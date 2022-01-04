d882df93402cfa8ba4d0076e8c6fe5e9ae14bb39
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-04 07:19:26 -0700

    TimeLeft method’un oluşturulması

--- IdentitySystemCore/TwoFactorService/TwoFactorService.cs
+++ IdentitySystemCore/TwoFactorService/TwoFactorService.cs
@@ -3,22 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace IdentitySystemCore.TwoFactorService
 {
     public class TwoFactorService
     {
         private readonly UrlEncoder _urlEncoder;
+        private readonly TwoFactorOptions _twoFactorOptions;
 
-        public TwoFactorService(UrlEncoder urlEncoder)
+        public TwoFactorService(UrlEncoder urlEncoder, IOptions<TwoFactorOptions> options)
         {
             _urlEncoder = urlEncoder;
+            _twoFactorOptions = options.Value;
         }
 
         public int GetCodeVerification()
         {
             Random random = new Random();
             return random.Next(1000, 9999);
+        }
+
+        public int TimeLeft(HttpContext httpContext)
+        {
+            if (httpContext.Session.GetString("currentTime") == null)
+            {
+                httpContext.Session.SetString("currentTime",DateTime.Now.AddSeconds(_twoFactorOptions.CodeTimeExpire).ToString());
+
+            }
+            DateTime currentTime = DateTime.Parse(httpContext.Session.GetString("currentTime").ToString());
+
+            int timeLeft = (int) (currentTime - DateTime.Now).TotalSeconds;
+
+            if (timeLeft <= 0)
+            {
+                httpContext.Session.Remove("currentTime");
+                return 0;
+            }
+            else
+            {
+               return timeLeft;
+            }
         }
 
         public string GenerateQrCodeUri(string email, string unformattedKey)
